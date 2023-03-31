896c41f1ff79e8735e9ed970a2d15730931a9715
homerabbit <me92years@gmail.com>
2023-03-31 19:35:40 +0900

    fix: DownloadAPI / API 수정 및 이미지 폴더 위치 변경

--- moviedb/src/main/java/org/sample/moviedb/api/DownloadAPI.java
+++ moviedb/src/main/java/org/sample/moviedb/api/DownloadAPI.java
@@ -3,16 +3,13 @@ package org.sample.moviedb.api;
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,31 +18,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.log4j.Log4j2;
-
 @RestController
 @RequestMapping("/api")
-@Log4j2
 public class DownloadAPI {
 
-	@Autowired
-	private ResourceLoader resourceLoader;
+	@Value("${org.sample.moviedb.server}")
+	private String SERVER;
 	
 	@GetMapping("/thumbnail")
 	public ResponseEntity<byte[]> thumbnail(String img) throws IOException, URISyntaxException {
 		try {
 			String uri = URLDecoder.decode(img, "utf-8");
-			Resource resource = resourceLoader.getResource("classpath:/static/img" + uri);
-			log.info("[리소스의 URL] > " + resource.getURL());
-			File file = resource.getFile();
-			log.info("[File] > " + file.getPath());
+			Path path = Paths.get(SERVER + "/img" + uri);
+			File file = path.toFile();
 			HttpHeaders header = new HttpHeaders();
 			header.add("Content-Type", Files.probeContentType(file.toPath()));
 			return new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		return null;
 	}
 	
 }
